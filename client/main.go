61b0d7cc2cfde2402f2019c2499cc39db4ba3beb
sakura1116vg <daisuke_sasaki@voyagegroup.com>
2018-03-13 14:25:01 +0900

    first commit

---
+++ client/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	pb "github.com/sakura1116vg/try_grpc/"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	// gRPCサーバとのコネクションを作成している
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultName
+	if len(os.Args) == 1 {
+		name = os.Args[1]
+	}
+	// gRPCサーバのSayHelloメソッドを呼び出している
+	r, err := c.SayHello(context.Background(), pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
---
+++ server/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/sakura1116vg/try_grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+// server 構造体
+// この構造体にprotoファイルで定期したRPCのメソッドを実装していく
+type server struct{}
+
+// SayHello メソッド
+// protoファイルのservice部分に記載したRPCメソッドを実装する
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	// server構造体をGreeterとしてProtocol Bufferに登録する
+	pb.RegisterGreeterServer(s, server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
